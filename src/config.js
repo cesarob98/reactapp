9100845442912c6baa40340fdcb6b35ac100bdc6
cesarob98 <cesarob98@gmail.com>
2025-03-05 09:48:16 -0600

    changes to remind the local settings

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
     baseURL: 'https://django-env.eba-qkkpspsz.us-west-2.elasticbeanstalk.com',
+    // baseURL: 'http://localhost:8000', //4 local testing
   };
   
   export default config;
